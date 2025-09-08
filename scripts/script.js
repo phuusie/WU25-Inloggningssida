1eb4d3f4efab0b14ee95c09806e79e21b52c5e13
phuusie <vanphunguyen@me.com>
2025-09-08 18:58:03 +0200

    påbörjat kommentering

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,20 @@
+/* 
+    Satta variabler för inloggning 
+*/
 const username = "admin";
 const password = "123";
 
+/* 
+    Hämtar elementet med id "page-container" från HTML-dokumentet 
+*/
 const pageContainer = document.getElementById("page-container");
 
+/*
+    Funktion för att skapa ett HTML-element med specifika attribut.
+    Exemple: createElement("tag", {class: "my-class", id: "my-id");
+    Detta skapar ett element av typen "tag" med attributen class = "my-class", id = "my-id".
+    Genom att göra en sådan funktion kan vi undvika att upprepa samma kod flera gånger när vi skapar olika HTML-element.
+*/
 function createElement(tag, options = {}) {
     const element = document.createElement(tag);
         if (options.class) element.className = options.class;
@@ -16,33 +28,49 @@ function createElement(tag, options = {}) {
     return element;
 }
 
-if (
-    localStorage.getItem("currentUser") === username &&
-    localStorage.getItem("currentPassword") === password
-) {
-    welcomePage();
-} else {
-    renderLoginForm();
-}
+/* FUNKTIONER FÖR ATT MINIMERA REPETITIV KOD */
 
+/* 
+    Funktion för att skapa ett input-fält med matchande label 
+*/
 function createInputGroup(labelText, inputOptions) {
+    /*  
+        Anropar createElement för att skapa en div med klassen "form-group" 
+    */
     const group = createElement("div", { 
         class: "form-group" 
     });
 
+    /* 
+    Anropar createElement för att skapa en label med attributen for och text
+    */
     const label = createElement("label", {
         for: inputOptions.id,
         text: labelText
     });  
+
+    /* 
+        group är div elementet vi skapade ovan 
+        appendChild lägger vi till elementen i div elementet
+    */
     group.appendChild(label);
     group.appendChild(document.createElement("br"));
 
+    /* 
+        Anropar createElement för att skapa ett input-fält som vi kan sedan sätta olika attribut på 
+    */
     const input = createElement("input", inputOptions);
     group.appendChild(input);
 
+    /* 
+    Returnerar hela gruppen med label och input 
+    */
     return group;
 }
 
+/* 
+    Lades till bara för att skapa checkboxen "Kom ihåg mig" 
+*/
 function createRememberMe() {
     const label = createElement("label");
 
@@ -57,6 +85,9 @@ function createRememberMe() {
     return label;
 }
 
+/* 
+    Lades till bara för att skapa länken "Glömt lösenord?" 
+*/
 function createForgotPassword() {
     const label = createElement("label", { 
         id: "forgotPassword" 
@@ -71,7 +102,48 @@ function createForgotPassword() {
     return label;
 }
 
-function renderLoginForm() {
+/* FUNKTIONER FÖR INLOGGNING, UTLOGGNING OCH SIDOR */
+
+function logout() {
+  loginPage();
+
+  localStorage.removeItem("currentUser");
+  localStorage.removeItem("currentPassword");
+}
+
+function login() {
+  const inputUsername = document.getElementById("username").value.toLowerCase();
+  const inputPassword = document.getElementById("password").value;
+
+  if (inputUsername === username && inputPassword === password) {
+    welcomePage();
+    localStorage.setItem("currentUser", inputUsername);
+    localStorage.setItem("currentPassword", inputPassword);
+  } else {
+    errorPage();
+  }
+}
+
+/* 
+    Här kollar vi om användaren är redan inloggad och sparad i localStorage
+    Om nykel och värde i localStorage matchar de satta variablerna, så visas välkomstsidan direkt
+    annars visas inloggningssidan
+*/
+if (
+    localStorage.getItem("currentUser") === username &&
+    localStorage.getItem("currentPassword") === password
+) {
+    welcomePage();
+} else {
+    loginPage();
+}
+
+/* PAGE SEKTIONEN */
+
+/* 
+    Landningssidan / Inloggningssidan 
+*/
+function loginPage() {
     pageContainer.innerHTML = "";
 
     const pageBorder = createElement("div", { 
@@ -124,24 +196,6 @@ function renderLoginForm() {
     });
 }
 
-function logout() {
-    renderLoginForm();
-    localStorage.removeItem("currentUser");
-    localStorage.removeItem("currentPassword");
-}
-
-function login() { 
-    const inputUsername = document.getElementById("username").value.toLowerCase();
-    const inputPassword = document.getElementById("password").value;
-
-    if (inputUsername === username && inputPassword === password) {
-        welcomePage();
-        localStorage.setItem("currentUser", inputUsername);
-        localStorage.setItem("currentPassword", inputPassword);
-    } else {
-        errorPage();
-    }
-}
 
 function welcomePage() {
     pageContainer.innerHTML = "";
